3f54cc0d1401fca50905d4dec5bcbd79de758fbc
Christian KLAT <christian.klat@gmail.com>
2024-06-01 21:19:00 +0200

    changes to platform.sh

--- platform.sh
+++ platform.sh
@@ -21,7 +21,24 @@ case $TARGETPLATFORM in
         echo "arm-unknown-linux-musleabihf" > /.target
         echo "/usr/lib/aarch64-linux-gnu" > /.libdir
         ;;
+    "darwin/arm64")
+        echo "aarch64-apple-darwin" > /.target
+        echo "/usr/lib/aarch64-linux-gnu" > /.libdir
+        ;;
+    "darwin/amd64")
+        echo "x86_64-apple-darwin" > /.target
+        echo "/usr/lib/x86_64-linux-gnu" > /.libdir
+        ;;
+    "windows/arm64")
+        echo "aarch64-pc-windows-msvc" > /.target
+        echo "/usr/lib/aarch64-linux-gnu" > /.libdir
+        ;;
+    "windows/amd64")
+        echo "x86_64-pc-windows-msvc" > /.target
+        echo "/usr/lib/aarch64-linux-gnu" > /.libdir
+        ;;
     *)
         echo "x86_64-unknown-linux-musl" > /.target
         echo "/usr/lib/x86_64-linux-gnu" > /.libdir
 esac
+
